5fa829d8f55fa6a3f674938495c5254dc13456e5
Ben Hollamby <ben.hollamby@outlook.com>
2022-05-11 16:16:17 +1200

    basics

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// variables
+string word = "string";
+int num = 66;
+double decmal = 66.06;
+
+//boolean
+bool sith = true;
+bool jedi = false;
+
 Console.WriteLine("Chemical Brothers & The Crystal Method!");
+Console.WriteLine(word + " " + num + Environment.NewLine + decmal);
+Console.WriteLine(sith);
+Console.WriteLine(jedi);
