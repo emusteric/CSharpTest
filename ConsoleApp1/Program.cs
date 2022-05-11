e73b94156f37a8d40c923b675cfeca4869ebb5e4
Ben Hollamby <ben.hollamby@outlook.com>
2022-05-12 09:12:58 +1200

    datetime object

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,15 @@
 string word = "string";
 int num = 66;
 double decmal = 66.06;
+//date time
+DateTime now = DateTime.Now;
+DateTime today = DateTime.Today;
+int day = now.Day;
+int month = now.Month;
+int year = now.Year;
+int hours = now.Hour;
+int minutes = now.Minute;
+int seconds = now.Second;  
 
 //boolean
 bool sith = true;
@@ -12,3 +21,11 @@ Console.WriteLine("Chemical Brothers & The Crystal Method!");
 Console.WriteLine(word + " " + num + Environment.NewLine + decmal);
 Console.WriteLine(sith);
 Console.WriteLine(jedi);
+Console.WriteLine(now);
+Console.WriteLine(today);
+Console.WriteLine(day);
+Console.WriteLine(month);
+Console.WriteLine(year);
+Console.WriteLine(hours);
+Console.WriteLine(minutes);
+Console.WriteLine(seconds);
