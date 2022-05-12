61f65bea69ab97133653a92c6b1d50a59bf93e09
Ben Hollamby <ben.hollamby@outlook.com>
2022-05-12 12:44:04 +1200

    random

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,9 @@
 string word = "string";
 int num = 66;
 double decmal = 66.06;
+Console.WriteLine("Chemical Brothers & The Crystal Method!");
+Console.WriteLine(word + " " + num + Environment.NewLine + decmal);
+
 //date time
 DateTime now = DateTime.Now;
 DateTime today = DateTime.Today;
@@ -11,16 +14,7 @@ int month = now.Month;
 int year = now.Year;
 int hours = now.Hour;
 int minutes = now.Minute;
-int seconds = now.Second;  
-
-//boolean
-bool sith = true;
-bool jedi = false;
-
-Console.WriteLine("Chemical Brothers & The Crystal Method!");
-Console.WriteLine(word + " " + num + Environment.NewLine + decmal);
-Console.WriteLine(sith);
-Console.WriteLine(jedi);
+int seconds = now.Second;
 Console.WriteLine(now);
 Console.WriteLine(today);
 Console.WriteLine(day);
@@ -29,3 +23,18 @@ Console.WriteLine(year);
 Console.WriteLine(hours);
 Console.WriteLine(minutes);
 Console.WriteLine(seconds);
+Console.WriteLine("Today is " + today.ToLongDateString());
+Console.WriteLine("Tomorrow is " + today.AddDays(1).ToLongDateString());
+Console.WriteLine(Environment.UserName);
+
+//boolean
+bool sith = true;
+bool jedi = false;
+Console.WriteLine(sith);
+Console.WriteLine(jedi);
+
+//randon
+Random randomNumbers = new Random();
+int number1 = randomNumbers.Next(0, 4);
+Console.WriteLine(number1);
+
